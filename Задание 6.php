a60a32043f311cd4ee485cc800c866fd255a4403
Artem Safronov <artemkatawersfpd@gmail.com>
2025-01-21 01:51:13 +0500

    Задание готово

--- Задание 6.php
+++ Задание 6.php
@@ -1,3 +1,43 @@
 <?php
 
-echo "test 1";
+$textStorage = [];
+
+function add (string $title, string $text, array &$textStorage) : void
+{
+    $textStorage[] = ['title' => $title, 'text' => $text];
+}
+
+add('Тест 1', 'Тест 2', $textStorage);
+add('Тест 3', 'Тест 4', $textStorage);
+
+print_r($textStorage);
+
+function remove(int $textId, array &$textStorage)
+{
+    if (isset($textStorage[$textId])) {
+        unset($textStorage[$textId]);
+        return true;
+    }
+    return false;
+}
+
+var_dump(remove(0, $textStorage));
+var_dump(remove(5, $textStorage));
+
+print_r($textStorage);
+
+function edit(int $textId, string $title, string $text, array &$textStorage)
+{
+    if (isset($textStorage[$textId])) {
+        $textStorage[$textId]['title'] = $title;
+        $textStorage[$textId]['text'] = $text;
+        return true;
+    }
+    return false;
+}
+
+edit(1, 'Тестовый заголовок', 'Тестовый текст', $textStorage);
+
+print_r($textStorage);
+
+var_dump(edit(7, 'Заголовок 1313', 'Текст 1567', $textStorage));
